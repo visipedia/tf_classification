c85563dd29beb5299c41e3e99ae232d6f8e0e910
Grant Van Horn <gvanhorn@visipedia.org>
2017-02-26 08:49:12 -0800

    converting inputs to uint8 from the visualization script

--- visualize_train_inputs.py
+++ visualize_train_inputs.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 import argparse
 
 from matplotlib import pyplot as plt
+import numpy as np
 import tensorflow as tf
 
 from config.parse_config import parse_config_file
@@ -33,6 +34,13 @@ def visualize_train_inputs(tfrecords, cfg):
           input_type='visualize'
         )
 
+        # Convert float images to uint8 images
+        image_to_convert = tf.placeholder(dtype=tf.float32,
+                                          shape=[cfg.IMAGE_PROCESSING.INPUT_SIZE,
+                                                 cfg.IMAGE_PROCESSING.INPUT_SIZE, 3])
+        uint8_image = tf.image.convert_image_dtype(image_to_convert, dtype=tf.uint8)
+
+
         coord = tf.train.Coordinator()
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
@@ -48,26 +56,39 @@ def visualize_train_inputs(tfrecords, cfg):
             distorted_images = output['inputs']
             image_ids = output['ids']
             labels = output['labels']
-            text_labels = output['text_labels']
+            #text_labels = output['text_labels']
 
             for b in range(cfg.BATCH_SIZE):
 
                 original_image = original_images[b]
                 distorted_image = distorted_images[b]
+
+                if original_image.dtype != np.uint8:
+                    original_image = sess.run(uint8_image, {image_to_convert : original_image})
+
+                if distorted_image.dtype != np.uint8:
+                    distorted_image = sess.run(uint8_image, {image_to_convert : distorted_image})
+
                 image_id = image_ids[b]
                 label = labels[b]
-                text_label = text_labels[b]
+                #text_label = text_labels[b]
 
                 fig = plt.figure('Train Inputs')
+                st = fig.suptitle("Image: %s\nLabel: %d" % (image_id, label), fontsize=12)
+
                 plt.subplot(2, 1, 1)
                 plt.imshow(original_image)
-                plt.title("Original Image")
+                plt.title("Original")
                 plt.axis('off')
 
                 plt.subplot(2, 1, 2)
                 plt.imshow(distorted_image)
-                plt.title("Modified Image")
+                plt.title("Modified")
                 plt.axis('off')
+
+                # Shift the subplots down a bit to make room for the super title
+                st.set_y(0.95)
+                fig.subplots_adjust(top=0.75)
 
                 plt.show(block=False)
 
@@ -90,6 +111,10 @@ def parse_args():
     parser.add_argument('--config', dest='config_file',
                         help='Path to the configuration file',
                         required=True, type=str)
+
+    # parser.add_argument('--text_labels', dest='show_text',
+#                         help='Path to the configuration file',
+#                         action='store_true', default=False)
 
     args = parser.parse_args()
     return args
